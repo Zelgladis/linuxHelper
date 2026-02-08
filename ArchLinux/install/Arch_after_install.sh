4d5d12092b38dc24368c8121e8b040b37cdb13aa
Zelgladis <Cep908@gmail.com>
2026-02-09 03:38:58 +0400

    fix 2 arch after install

--- ArchLinux/install/Arch_after_install.sh
+++ ArchLinux/install/Arch_after_install.sh
@@ -308,7 +308,7 @@ fi
 
 if [[ " choice choice_min all " =~ " ${po_main_list[$po_main-1]} " ]];then
     echo "Add PO"
-    sudo pacman -S "$po_install[@]" --noconfirm
+    sudo pacman -S ${po_install[@]} --noconfirm
     flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo || true
     sudo usermod -aG docker $USER || true
 fi
