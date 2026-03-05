a2e695f696ba2fa43f3b460879901e0fc4d7ef1e
Zelgladis <Cep908@gmail.com>
2026-03-05 22:19:16 +0400

    Арчи

--- ArchLinux/install/Arch_install.sh
+++ ArchLinux/install/Arch_install.sh
@@ -74,7 +74,7 @@ echo "127.0.0.1 localhost
 pacman -Syu --noconfirm
 pacman -S vi nano reflector gcc perl make dhcpcd openssh \
        btrfs-progs e2fsprogs grub efibootmgr os-prober hwinfo bash-completion \
-       chafa libsixel imagemagick --noconfirm
+       chafa libsixel imagemagick ntfs-3g --noconfirm
 systemctl enable sshd
 systemctl enable dhcpcd
 
