a950db439c1a4549e67055836012c1c0568cdf20
Ilinium <Cep908@gmail.com>
2026-07-23 02:10:15 +0400

    noctalia

---
+++ ArchLinux/niri/install_noctalia.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+sudo pacman -Syu
+sudo pacman -S niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-desktop-portal-kde kitty matugen cava qt6-multimedia-ffmpeg waybar swaybg swayidle fuzzel
+
+paru -S noctalia-git
+
